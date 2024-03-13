9a554c355d3ada122c6dcb9ee3e58ca44d955587
Dmitry Ponyatov <dponyatov@gmail.com>
2024-03-13 21:25:41 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- inc/main.hpp
+++ inc/main.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 extern void arg(int argc, char argv[]);
 extern int main(int argc, char *argv[]);
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "main.hpp"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     arg(0, argv[0]);
     for (int i = 1; i < argc; i++) {  //
         arg(i, argv[i]);
+        FILE* src = NULL;
+        assert(src = fopen(argv[i], "r"));
+        fclose(src);
     }
 }
 
